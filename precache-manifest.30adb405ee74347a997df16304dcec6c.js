721e311887bce5f481fe007c2d261470b1adc110
Rachel Tieu <rachel.tieu02@myhunter.cuny.edu>
2020-06-14 15:27:44 -0400

    Updates

---
+++ precache-manifest.30adb405ee74347a997df16304dcec6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b735c9ef4be28a979639802796e29110",
+    "url": "/super1665-website/index.html"
+  },
+  {
+    "revision": "3cbe4ba17217a69cf8e1",
+    "url": "/super1665-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3edc45b1bbe9f29e2a66",
+    "url": "/super1665-website/static/js/2.595b3f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/super1665-website/static/js/2.595b3f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cbe4ba17217a69cf8e1",
+    "url": "/super1665-website/static/js/main.4f23a84e.chunk.js"
+  },
+  {
+    "revision": "f48d918fded1078dd3cf",
+    "url": "/super1665-website/static/js/runtime-main.20a77ad5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/super1665-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
